da273f03f78af00ad73822e8d7825df8b78a5868
Christopher Brown <cmbrown18.cs@gmail.com>
2021-09-13 09:55:24 -0400

    Login fucking works

--- capstone1/DjangoApp/views.py
+++ capstone1/DjangoApp/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Processed, User, UserManager
+from .models import Processed, User, UserManager, set_name
 from .forms import SignUpForm, LoginForm
-from django.contrib.auth import authenticate, login
 from .controller import Controller
 from .ui import ConsoleUI
 from django.contrib.auth.models import User
-from passlib.hash import pbkdf2_sha256
 
 # Create your views here.
 # from capstone1.DjangoApp.forms import PolicyForm
@@ -40,16 +38,14 @@ def user(request):
 
 
 def display_user(request):
-    output = Person.objects.all()
+    output = User.objects.all()
     return render(request, "dis_user.html", {'output': output})
 
 
 def create_user(request):
     if request.method == 'POST':
-        new_user = Person()
-        new_user.username = request.POST.get('user')
+        new_user = UserManager.create_user(request.POST.get('user'), request.POST.get('password'))
         new_user.fullname = request.POST.get('fullname')
-        new_user.password = request.POST.get('password')
         new_user.save()
     return render(request, "create_user.html")
 
@@ -60,7 +56,7 @@ def mod_user(request):
 
 def del_user(request):
     if request.method == 'POST':
-        query = Person.objects.get(username=request.POST.get('user'))
+        query = User.objects.get(username=request.POST.get('user'))
         query.delete()
     return render(request, "del_user.html")
 
@@ -68,13 +64,9 @@ def del_user(request):
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        user_info = Person()
-        user_info.username = request.POST.get('username')
+        user_info = UserManager.create_user(UserManager(), request.POST.get('username'), request.POST.get('password'))
 
-        password = pbkdf2_sha256.hash(request.POST.get('password'))
-        user_info.password = password
-        user_info.first_name = request.POST.get('first_name')
-        user_info.last_name = request.POST.get('last_name')
+        set_name(user_info, request.POST.get('first_name'), request.POST.get('last_name'))
         user_info.save()
 
         if form.is_valid():
@@ -97,18 +89,18 @@ def login(request):
         print(user_value)
         print(password_value)
 
-        if Person.objects.filter(username=user_value).exists():
+        if User.objects.filter(username=user_value).exists():
             print("I got into the if statement")
-            person = Person.get_person
-            if pbkdf2_sha256.verify(password_value, person.password):
+            the_user = User.objects.get(username=user_value)
+            if the_user.check_password(password_value):
                 context = {'form': form, 'error': 'The login has been successful'}
+                return render(request, 'index.html', context)
+            else:
+                context = {'form': form, 'error': 'The username and password combination is incorrect'}
                 return render(request, 'login.html', context)
         else:
-            context = {'form': form,
-                       'error': 'The username and password combination is incorrect'}
-
-            return render(request, 'login.html', context)
-
+            print("Could not find user")
     else:
         context = {'form': form}
         return render(request, 'login.html', context)
+
